26dbc8815f9abe689d2578bdf91cdfa6159dc99d
vladimirturbin <Solderingirontip>
2020-07-02 17:45:47 +0300

    still calculator

--- Calculator/my.py
+++ Calculator/my.py
@@ -1,3 +1,18 @@
 class Calculator(object):
-  def evaluate(self, string):
-    return 0
+    def evaluate(self, string):
+        elements = string.split(' ')
+
+        while '(' in elements:
+            pass
+
+        while '*' in elements:
+            for i in range(len(elements)):
+                if elements[i] == '*':
+                    str(float(elements[i-1]) * float(elements[i+1]))
+                    elements
+        return elements
+
+
+if __name__ == '__main__':
+    calculator = Calculator()
+    print(calculator.evaluate('1 + 1'))
