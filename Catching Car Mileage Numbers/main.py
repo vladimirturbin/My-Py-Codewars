5af80438a7eaebdb1494df1a82f25fd52d8b2a0c
Vladimir Turbin <vladimirturbin@gmail.com>
2020-07-21 21:59:43 +0300

    Catching Car Mileage Numbers done

--- Catching Car Mileage Numbers/main.py
+++ Catching Car Mileage Numbers/main.py
@@ -1,2 +1,51 @@
 def is_interesting(number, awesome_phrases):
-    pass
+    if interesting(number, awesome_phrases):
+        return 2
+    if interesting(number + 1, awesome_phrases) \
+            or interesting(number + 2, awesome_phrases):
+        return 1
+    return 0
+
+
+def interesting(number, awesome_phrases):
+    number_str = str(number)
+    number_list = [int(i) for i in number_str]
+
+    if number < 100:
+        return False
+
+    if number in awesome_phrases:
+        return True
+
+    if str(number) == number_str[::-1]:
+        return True
+
+    result = True
+    for i in range(len(number_list) - 1):
+        if number_list[i] != (number_list[i + 1] + 1) % 10:
+            result = False
+    if 0 in number_list[:-1]:
+        result = False
+    if result:
+        return True
+
+    result = True
+    for i in range(len(number_list) - 1):
+        if number_list[i + 1] != (number_list[i] + 1) % 10:
+            result = False
+    if 0 in number_list[:-1]:
+        result = False
+    if result:
+        return True
+
+    if len(set(number_list)) == 1:
+        return True
+
+    if set(number_list[1:]) == {0}:
+        return True
+
+    return False
+
+
+if __name__ == '__main__':
+    print(is_interesting(109, [31337]))
